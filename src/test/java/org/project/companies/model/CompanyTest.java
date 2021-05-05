1192ea9e57e76a554ab078d875a399d088bea4a5
aaronreilly00 <you@example.com>
2021-05-05 18:30:10 +0100

    adding unit tests

---
+++ src/test/java/org/project/companies/model/CompanyTest.java
@@ -0,0 +1,59 @@
+package org.project.companies.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CompanyTest {
+
+	//create Company instance
+	Company company = new Company(3,"MobileCafe","23 oldtown road, whitehall","D11","Hospitality","Looking for open space to serve the public");
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetCompanyName() {
+		String expectCompanName = "MobileCafe";
+		String actualCompanyName = company.getCompanyName();
+		assertEquals(expectCompanName,actualCompanyName);
+	}
+
+	@Test
+	public void testGetAddress() {
+		String expectAddress = "23 oldtown road, whitehall";
+		String actualAddress = company.getAddress();
+		assertTrue(expectAddress.contentEquals(actualAddress));
+	}
+
+	@Test
+	public void testGetPostcode() {
+		String expectPostcode = "D11";
+		String actualPostcode = company.getPostcode();
+		assertEquals(expectPostcode,actualPostcode);
+	}
+
+	@Test
+	public void testGetSector() {
+		String expectSector = "Hospitality";
+		String actualSector = company.getSector();
+		assertEquals(expectSector,actualSector);
+	}
+
+	@Test
+	public void testGetDetails() {
+		String expectDetails = "Looking for open space to serve the public";
+		String actualDetails = company.getDetails();
+		assertEquals(expectDetails,actualDetails);
+
+	}
+
+
+}
