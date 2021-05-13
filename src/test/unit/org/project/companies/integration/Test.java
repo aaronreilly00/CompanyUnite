e7a4c1286471fcdf652d2e4994ff7d7e33a22dc0
aaronreilly00 <you@example.com>
2021-05-13 21:15:43 +0100

    Updating connection to the clearDB database used for API requests

---
+++ src/test/unit/org/project/companies/integration/Test.java
@@ -0,0 +1,23 @@
+package org.project.companies.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
